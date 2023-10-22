68adbd2892ac893bb1f5d49f3a5d6c89343712d9
Kosay Jabre <kosay.jabre@gmail.com>
2023-10-22 13:07:13 -0700

    Minor CSS changes

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@
         <p> I'm a geek with an affinity for machine learning and distributed systems. </p>
         <p> I currently work as a Software Engineer at Meta, where I integrate cutting-edge artificial intelligence systems into products. </p>
         <p> Outside of work I enjoy reading, writing, and playing video games. </p>
-        <p> Check out my <a href="/blog">blog.</a></p>
+        <p> Check out my <a href="/blog">blog!</a></p>
 
     </main>
     <?php include "./templates/footer.php"; ?>
--- src/templates/nav.php
+++ src/templates/nav.php
@@ -1,7 +1,13 @@
-<nav>
-    <h1 id="hero"><a href="/">Kosay Jabre</a></h1>
-    <ul>
+<?php
+$classes = "grow-on-hover";
+if (str_contains($_SERVER['SCRIPT_NAME'], "/blog/")) {
+    $classes .= " navbar-active";
+}
+?>
 
-        <li><a <?php if (str_contains($_SERVER['SCRIPT_NAME'], "/blog/")) { ?> class="navbar-active" <?php } ?> href="/blog">Blog</a></li>
+<nav>
+    <h1 id="hero" class="grow-on-hover"><a href="/">Kosay Jabre</a></h1>
+    <ul>
+        <li class="grow-on-hover"><a class="<?php echo $classes; ?>" href="/blog">Blog</a></li>
     </ul>
 </nav>
