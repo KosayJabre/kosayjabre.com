d2d0ee54bf1247d0f0ab7275facfddee42147df8
Kosay Jabre <kosay.jabre@gmail.com>
2023-10-21 22:36:38 -0700

    Updated front page copy

--- src/index.php
+++ src/index.php
@@ -8,8 +8,11 @@
 <body>
     <?php include "./templates/nav.php"; ?>
     <main>
-        <h2 id="about">About Me</h2>
-        <p> I am a software engineer with an affinity for machine learning and distributed systems. </p>
+        <p> I'm a geek with an affinity for machine learning and distributed systems. </p>
+        <p> I currently work as a Software Engineer at Meta, where I integrate cutting-edge artificial intelligence systems into products. </p>
+        <p> Outside of work I enjoy reading, writing, and playing video games. </p>
+        <p> Check out my <a href="/blog">blog.</a></p>
+
     </main>
     <?php include "./templates/footer.php"; ?>
 </body>
