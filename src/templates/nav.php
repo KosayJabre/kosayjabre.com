15a8cad0b28f5b03d2a492fc149e67143baee22c
Kosay Jabre <kosay.jabre@gmail.com>
2023-10-21 22:43:14 -0700

    Underline active page in navbar

--- src/templates/nav.php
+++ src/templates/nav.php
@@ -1,6 +1,7 @@
 <nav>
     <h1 id="hero"><a href="/">Kosay Jabre</a></h1>
     <ul>
-        <li><a href="/blog">Blog</a></li>
+
+        <li><a <?php if (str_contains($_SERVER['SCRIPT_NAME'], "/blog/")) { ?> class="navbar-active" <?php } ?> href="/blog">Blog</a></li>
     </ul>
 </nav>
