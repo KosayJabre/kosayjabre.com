3379815c4b43218d761523f5e5299c76ea9bf721
Kosay Jabre <kosay.jabre@gmail.com>
2023-10-22 13:15:58 -0700

    Use absolute paths instead of relative ones

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,22 +1,21 @@
 <footer>
     <div id="footer-content">
-        <a href="https://github.com/KosayJabre/kosayjabre.com" target="_blank" title="Source code for this site"
-            id="source-link">Source code for this site</a>
+        <a href="https://github.com/KosayJabre/kosayjabre.com" target="_blank" title="Source code for this site" id="source-link">Source code for this site</a>
         <div id="social-media-icons">
             <a href="mailto: kosay.jabre@gmail.com" title="Contact me">
-                <img src="icons/email.svg" alt="Contact me" class="icon grow-on-hover">
+                <img src="/icons/email.svg" alt="Contact me" class="icon grow-on-hover">
             </a>
             <a href="https://www.linkedin.com/in/kosay-jabre/" target="_blank" title="My LinkedIn">
-                <img src="icons/linkedin.svg" alt="LinkedIn logo" class="icon grow-on-hover">
+                <img src="/icons/linkedin.svg" alt="LinkedIn logo" class="icon grow-on-hover">
             </a>
             <a href="https://github.com/KosayJabre" target="_blank" title="My GitHub">
-                <img src="icons/github.svg" alt="GitHub logo" class="icon grow-on-hover">
+                <img src="/icons/github.svg" alt="GitHub logo" class="icon grow-on-hover">
             </a>
             <a href="https://www.facebook.com/kosay.j.jabre/" target="_blank" title="My Facebook">
-                <img src="icons/facebook.svg" alt="Facebook logo" class="icon grow-on-hover">
+                <img src="/icons/facebook.svg" alt="Facebook logo" class="icon grow-on-hover">
             </a>
             <a href="https://stackoverflow.com/users/13707960/kosay-jabre" target="_blank" title="My Stack Overflow">
-                <img src="icons/stack_overflow.svg" alt="Stack Overflow logo" class="icon grow-on-hover">
+                <img src="/icons/stack_overflow.svg" alt="Stack Overflow logo" class="icon grow-on-hover">
             </a>
         </div>
     </div>
