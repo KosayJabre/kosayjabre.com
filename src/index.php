fde7faf364e819ff0c215efc18983610380105a3
Kosay Jabre <kosay.jabre@gmail.com>
2023-11-05 15:15:15 -0800

    Fix issue where scrollbar appears despite content being short

--- src/index.php
+++ src/index.php
@@ -8,11 +8,12 @@
 <body>
     <?php include "./templates/nav.php"; ?>
     <main>
+        <div>
         <p> I'm a geek with an affinity for machine learning and distributed systems. </p>
         <p> I currently work as a Software Engineer at Meta, where I integrate cutting-edge artificial intelligence systems into products. </p>
         <p> Outside of work I enjoy reading, writing, and playing video games. </p>
         <p> Check out my <a href="/blog">blog!</a></p>
-
+        </div>
     </main>
     <?php include "./templates/footer.php"; ?>
 </body>
