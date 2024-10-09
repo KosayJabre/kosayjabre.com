0abcea6e930f647b5cda7276d35bb56c53e9259c
Kosay Jabre <kosay.jabre@gmail.com>
2024-10-09 11:15:08 +0200

    Updated blog header to be h2 instead of h1

--- src/templates/blog_header.php
+++ src/templates/blog_header.php
@@ -1,7 +1,7 @@
 <header id="blog-post-header">
-    <h1 id="blog-post-title">
+    <h2 id="blog-post-title">
         <?php echo $metadata['title']; ?>
-    </h1>
+    </h2>
     <div id="blog-post-date">
         <?php echo $metadata['date']; ?>
     </div>
